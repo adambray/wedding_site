8c745cb67f5c278d3e56f38919ea6e0a4d28902a
Adam Bray <adam.bray@yale.edu>
2019-05-19 19:04:38 -0400

    Update with registry and locations

--- map.js
+++ map.js
@@ -1,143 +1,165 @@
 var map;
 
 function initMap() {
-    const reception = {
-        location: {lat:  38.906330, lng: -77.023811},
-        title: "Ceremony and Reception",
-        popupContent: '<h4>Ceremony and Reception</h4>'
-    };
+  const reception = {
+    location: {lat: 38.906330, lng: -77.023811},
+    title: "Ceremony and Reception",
+    popupContent: '<h4>Ceremony and Reception</h4>'
+  };
 
-    const brunch = {
-        location: {lat:  38.879125, lng: -77.005319 },
-        title: "Sunday Brunch",
-        popupContent: '<h4>Sunday Brunch</h4>'
-    };
+  const brunch = {
+    location: {lat: 38.879125, lng: -77.005319},
+    title: "Sunday Brunch",
+    popupContent: '<h4>Sunday Brunch</h4>'
+  };
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat:  38.895296, lng: -77.016018 },
-        styles: [
-            {
-                "featureType": "administrative.land_parcel",
-                "elementType": "labels",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "poi",
-                "elementType": "labels.text",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "poi.attraction",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "poi.business",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "poi.government",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "poi.medical",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "poi.park",
-                "elementType": "labels.text",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "poi.place_of_worship",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "poi.school",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "poi.sports_complex",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "road.local",
-                "elementType": "labels",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            }
-        ],
-        zoom: 13
-    });
 
-    const receptionInfoWindow = new google.maps.InfoWindow({
-        content: reception.popupContent,
-    });
+  const drinks = {
+    location: {lat: 38.900034, lng: -77.021255},
+    title: "Welcome Drinks",
+    popupContent: '<h4>Welcome Drinks - Crimson Whiskey Bar</h4>'
+  };
 
-    const receptionMarker = new google.maps.Marker({
-        position: reception.location,
-        map: map,
-        title: 'Reception'
-    });
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 38.895296, lng: -77.016018},
+    styles: [
+      {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.attraction",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.business",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.government",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.medical",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.place_of_worship",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.school",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.sports_complex",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      }
+    ],
+    zoom: 13
+  });
 
-    receptionMarker.addListener('click', function () {
-        receptionInfoWindow.open(map, receptionMarker);
-    });
+  const receptionInfoWindow = new google.maps.InfoWindow({
+    content: reception.popupContent,
+  });
 
-    const brunchInfoWindow = new google.maps.InfoWindow({
-        content: brunch.popupContent,
-    });
+  const receptionMarker = new google.maps.Marker({
+    position: reception.location,
+    map: map,
+    title: 'Reception'
+  });
 
-    const brunchMarker = new google.maps.Marker({
-        position: brunch.location,
-        map: map,
-        title: 'Reception'
-    });
+  receptionMarker.addListener('click', function () {
+    receptionInfoWindow.open(map, receptionMarker);
+  });
 
-    brunchMarker.addListener('click', function () {
-        brunchInfoWindow.open(map, brunchMarker);
-    });
+  const brunchInfoWindow = new google.maps.InfoWindow({
+    content: brunch.popupContent,
+  });
+
+  const brunchMarker = new google.maps.Marker({
+    position: brunch.location,
+    map: map,
+    title: 'Reception'
+  });
+
+  brunchMarker.addListener('click', function () {
+    brunchInfoWindow.open(map, brunchMarker);
+  });
+
+  const drinksInfoWindow = new google.maps.InfoWindow({
+    content: drinks.popupContent,
+  });
+
+  const drinksMarker = new google.maps.Marker({
+    position: drinks.location,
+    map: map,
+    title: 'Reception'
+  });
+
+  drinksMarker.addListener('click', function () {
+    drinksInfoWindow.open(map, drinksMarker);
+  });
+
 }
 
